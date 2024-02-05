ef079fb5f49096305719d795d7c9a0ac9e1ce152
ortega <kabweortega@gmail.com>
2024-02-05 08:29:02 +0200

    Build: Add spring cloud routing dependency

--- src/main/java/com/ortega/slusambo/SLusamboController.java
+++ src/main/java/com/ortega/slusambo/SLusamboController.java
@@ -1,4 +1,4 @@
-package com.ortega.slusambo.controllers;
+package com.ortega.slusambo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
